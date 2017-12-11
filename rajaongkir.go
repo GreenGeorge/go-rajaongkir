08cf9d0b4789f780a4b905b86056163a969e72fc
George Ananda <george@polymathco.com>
2017-12-11 20:01:33 +0700

    Fix mistaken json identifier

--- rajaongkir.go
+++ rajaongkir.go
@@ -57,7 +57,7 @@ type Province struct {
 // City stores the details of a city
 type City struct {
 	CityID     string `json:"city_id"`
-	ProvinceID string `json:"city_id"`
+	ProvinceID string `json:"province_id"`
 	Province   string `json:"province"`
 	Type       string `json:"type"`
 	CityName   string `json:"city_name"`
